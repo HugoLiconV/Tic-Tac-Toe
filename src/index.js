62f434f348cfee40b4d88f2152fe2033c4ec9f9f
Hugo <hugo_val28@hotmail.com>
2017-08-03 13:44:44 -0600

    grid layout with flex

---
+++ src/index.js
@@ -0,0 +1,175 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+// class Square extends React.Component {
+/* 
+  In JavaScript classes, you need to explicitly call super(); when defining the 
+  constructor of a subclass. */
+
+/* 
+Note that onClick on the DOM <button> component has a special 
+meaning to React, but we could have called onClick prop in 
+Square and handleClick in Board something else. It is, 
+however, a common convention in React apps to use on* names 
+for the handler prop names and handle* for their 
+implementations.
+ */
+
+/* 
+  Debido a que removimos el constructor de Square y ahora
+  este solo consiste del metodo render podemos usar un
+  Functional Component en lugar de una clase
+ */
+function Square(props) {
+  return (
+    <button className="square" 
+      onClick= { props.onClick }>
+      { props.value }
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    /*se hace una copia del array porque immutability
+    es importante: es importante porque permite
+    trackear cambios, deshacer y rehacer versiones de datos
+    y permite a React determinar que re-renderizar puesto
+    que puede comparar el objeto original con el cambio
+    */
+    const squares = [...current.squares];
+
+    //return early and ignore the click if someone has already
+    //won the game or if a square is already filled:
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Move #" + move : "Game start";
+      return (
+        <li key={move}>
+          <a href="#" onClick={() => this.jumpTo(move)}>
+            {desc}
+          </a>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>
+            {moves}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
